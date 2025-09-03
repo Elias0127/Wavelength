f08b22e202943f712b7d166d9797038669cf8803
Elias0127 <eliasmekonen001@gmail.com>
2025-09-03 05:05:44 -0400

    Initial Commit

---
+++ Wavelength/WavelengthApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavelengthApp.swift
+//  Wavelength
+//
+//  Created by Elias Woldie on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WavelengthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WavelengthTests/WavelengthTests.swift
@@ -0,0 +1,17 @@
+//
+//  WavelengthTests.swift
+//  WavelengthTests
+//
+//  Created by Elias Woldie on 9/3/25.
+//
+
+import Testing
+@testable import Wavelength
+
+struct WavelengthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
